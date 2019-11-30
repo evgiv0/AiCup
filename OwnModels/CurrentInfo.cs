858bed8f32a036b8694c6aa08eec1fa6dc56724a
evgiv0 <zhe956@gmail.com>
2019-11-30 21:14:16 +0300

    Update CurrentInfo.cs

--- OwnModels/CurrentInfo.cs
+++ OwnModels/CurrentInfo.cs
@@ -8,6 +8,7 @@ namespace AiCup2019.OwnModels
         public Unit Me { get; set; }
         public Unit? Enemy { get; set; }
         public LootBox? NearestWeapon { get; set; }
+        public LootBox? NearestNotBazuka { get; internal set; }
         public LootBox? NearestHealth { get; set; }
         public LootBox? NearestMine { get; set; }
         public LootBox? BestWeapon { get; set; }
